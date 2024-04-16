5d9c9fb910f81afd29795a52de554cc81f9e214d
Schonarthcams <105643168+SCHONARTHCAMS@users.noreply.github.com>
2024-04-16 10:59:52 -0300

    .

--- main.js
+++ main.js
@@ -20,4 +20,6 @@ let segundos = Math.floor(tempoFinal / 1000);
 let minutos = Math.floor(segundos / 60);
 let horas  = Math.floor(minuto / 60);
 let dias  = Math.floor(horas / 24);
+
+return dias + "dias" + horas + "horas" + minutos + "minutos" +segundos + "segundos";
 }
