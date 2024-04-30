b11729cdb6dd31eb0a0575d481c7b2be0fe73543
Schonarthcams <105643168+SCHONARTHCAMS@users.noreply.github.com>
2024-04-30 10:30:32 -0300

    .

--- main.js
+++ main.js
@@ -13,10 +13,10 @@ for (let i = 0; i<botoes.length; i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-01-10T00:00:00");
-const tempoObjetivo1 = new Date("2025-01-10T00:00:00");
-const tempoObjetivo1 = new Date("2025-12-13T00:00:00");
-const tempoObjetivo1 = new Date("2025-05-30T00:00:00");
+const tempoObjetivo1 = new Date("2025-01-10T23:59:59");
+const tempoObjetivo1 = new Date("2025-01-10T23:59:59");
+const tempoObjetivo1 = new Date("2025-12-13T23:59:59");
+const tempoObjetivo1 = new Date("2025-05-30T23:59:59");
 function calculaTempo(tempoObjetivo){
 let tempoAtual = new Date();
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
